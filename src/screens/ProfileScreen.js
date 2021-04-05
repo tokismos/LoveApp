16abade0ea4ecd18d0b7829eef396207dde2805d
Tokismos <tokismos@gmail.com>
2021-04-05 23:14:42 +0100

    adding modal

--- src/screens/ProfileScreen.js
+++ src/screens/ProfileScreen.js
@@ -4,37 +4,97 @@ import {
   Text,
   View,
   Dimensions,
-  Modal,
-  Button,
   TouchableOpacity,
 } from "react-native";
-import ListMood from "../components/ListMood";
-import { useSharedValue } from "react-native-reanimated";
+import { PanGestureHandler } from "react-native-gesture-handler";
+import Animated, {
+  useSharedValue,
+  useAnimatedStyle,
+  useAnimatedGestureHandler,
+  withSpring,
+  runOnJS,
+} from "react-native-reanimated";
 
-const Width = Dimensions.get("window").width;
+const SPRING_CONFIG = {
+  damping: 80,
+  overshootClamping: true,
+  restDisplacementThreshold: 0.1,
+  restSpeedThreshold: 0.1,
+  stiffness: 500,
+};
 const Height = Dimensions.get("window").height;
 
 const Profile = () => {
+  const top = useSharedValue(Height);
+  const [isBig, setIsBig] = useState(false);
+  const modalStyle = useAnimatedStyle(() => {
+    return {
+      top: withSpring(top.value, SPRING_CONFIG),
+    };
+  });
+  const eventHandler = useAnimatedGestureHandler({
+    onStart: (event, ctx) => {
+      ctx.value = top.value;
+    },
+    onActive: (event, ctx) => {
+      if (top.value == 200 && event.translationY < 0) {
+        //stoper le defilement vers le haut quand le modal est ouvert
+        return;
+      }
+      top.value = ctx.value + event.translationY;
+      console.log(event.translationY);
+    },
+    onEnd: (event, ctx) => {
+      if (event.translationY < -Height / 5) {
+        top.value = 200;
+        runOnJS(setIsBig)(true);
+      } else if (event.translationY > Height / 5) {
+        top.value = Height - 150;
+        runOnJS(setIsBig)(false);
+      } else {
+        if (isBig) {
+          //pour retourner au state precedent du modal si on swipe une petite distance
+          top.value = 200;
+        } else {
+          top.value = Height - 150;
+        }
+      }
+    },
+  });
   return (
-    <View style={styles.container}>
-      <View style={styles.topContainer}>
-        <View style={styles.image}></View>
-        <Text>hiii</Text>
+    <>
+      <View style={styles.container}>
+        <View style={styles.topContainer}>
+          <View style={styles.image}></View>
+          <Text>hiii</Text>
+        </View>
+        <View style={styles.botBackContainer}>
+          <View style={styles.botContainer}></View>
+        </View>
+        <TouchableOpacity
+          style={styles.moodButton}
+          onPress={() => (top.value = Height - 150)}
+        ></TouchableOpacity>
+        <PanGestureHandler onGestureEvent={eventHandler}>
+          <Animated.View style={[styles.modalView, modalStyle]}></Animated.View>
+        </PanGestureHandler>
       </View>
-      <View style={styles.botBackContainer}>
-        <View style={styles.botContainer}></View>
-      </View>
-      <TouchableOpacity
-        style={styles.moodButton}
-        onPress={() => setIsVisible(true)}
-      ></TouchableOpacity>
-    </View>
+    </>
   );
 };
 
 export default Profile;
 
 const styles = StyleSheet.create({
+  modalView: {
+    position: "absolute",
+    backgroundColor: "black",
+    bottom: 0,
+    right: 0,
+    left: 0,
+    borderTopLeftRadius: 20,
+    borderTopRightRadius: 20,
+  },
   container: { flex: 1 },
   topContainer: {
     backgroundColor: "blue",
